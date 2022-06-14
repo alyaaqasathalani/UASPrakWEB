f96c448f8d6e359eadc840f3061b6f5344c0864d
Alyaa Qasthalani <alyaaqasthalani@upi.edu>
2022-06-14 16:10:14 +0700

    UAS

---
+++ Kelompok1_MasakApa/includes/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','localhost:8889');
+define('DB_USER','root');
+define('DB_PASS' ,'root');
+define('DB_NAME','recipe');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
